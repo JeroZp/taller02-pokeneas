7d6a24be140049cfe9049a0841c58e691c1730f0
Jeronimo Perez Baquero <perezjeronimo35@gmail.com>
2025-05-13 23:36:10 -0500

    Complete app to review with mate

--- src/routes/randomPokenea.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRandomPokenea } = require('../controllers/pokeneasController');
-
-router.get('/', getRandomPokenea);
-
-module.exports = router;
